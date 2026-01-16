e854102c5c1496f5bf7d173691c648d766ba0e41
Quek Zhi Qiang <zhi_qiang_quek@aiap.sg>
2026-01-16 22:19:39 +0800

    Updated img src

--- utils/map_utils.py
+++ utils/map_utils.py
@@ -19,5 +19,5 @@ def get_onemap_attribution() -> str:
     Returns:
         String containing HTML attribution with OneMap logo, link, and SLA link
     """
-    return '''<img src="https://www.onemap.gov.sg/web-assets/images/logo/om_logo.png" style="height:20px;width:20px;"/>&nbsp;<a href="https://www.onemap.gov.sg/" target="_blank" rel="noopener noreferrer">OneMap</a>&nbsp;&copy;&nbsp;contributors&nbsp;&#124;&nbsp;<a href="https://www.sla.gov.sg/" target="_blank" rel="noopener noreferrer">Singapore Land Authority</a>'''
+    return '''<img src="/assets/img/om_logo.png" style="height:20px;width:20px;"/>&nbsp;<a href="https://www.onemap.gov.sg/" target="_blank" rel="noopener noreferrer">OneMap</a>&nbsp;&copy;&nbsp;contributors&nbsp;&#124;&nbsp;<a href="https://www.sla.gov.sg/" target="_blank" rel="noopener noreferrer">Singapore Land Authority</a>'''
 
